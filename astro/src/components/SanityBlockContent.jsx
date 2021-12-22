cc95f7d6af2330218f4583b8c0499a9c5b27b841
Jaydan Urwin <jaydan@jaydanurwin.com>
2021-12-22 16:34:55 -0700

    added code block serializer

--- astro/src/components/SanityBlockContent.jsx
+++ astro/src/components/SanityBlockContent.jsx
@@ -1,8 +1,14 @@
 import {h} from 'preact';
 import BlockContent from '@sanity/block-content-to-react';
 
+const serializers = {
+  types: {
+    code: ({node}) => <code-block code={node.code} language={node.language}></code-block>
+  }
+}
+
 export default function SanityBlockContent({blocks}) {
   return (
-    <BlockContent blocks={blocks} />
+    <BlockContent blocks={blocks} serializers={serializers} />
   )
 }
