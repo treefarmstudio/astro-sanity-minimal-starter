6a13db2c73c514b01a40614dbbde23e104c99505
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-01-30 19:48:02 -0700

    moving to sanity client and groq

--- astro/src/lib/sanityClient.js
+++ astro/src/lib/sanityClient.js
@@ -1,6 +1,11 @@
-export const client = {
-    projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-    dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-    token: import.meta.env.PUBLIC_SANITY_READ_TOKEN,
-    useCdn: true,
+import sanityClient from '@sanity/client';
+
+const config = {
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  token: import.meta.env.PUBLIC_SANITY_READ_TOKEN,
+  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION,
+  useCdn: true,
 };
+
+export const client = sanityClient(config);
