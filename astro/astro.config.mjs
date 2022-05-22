61d6bd021a518846b2d74f77ab31a745af9c4638
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-05-21 22:54:09 -0600

    cleanup

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://astro-sanity-minimal-starter.netlify.app/' // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  ,
+  site: 'https://astro-sanity-minimal-starter.netlify.app/', // Your public domain, e.g.: https://my-site.dev/
   integrations: [sitemap()]
 });
