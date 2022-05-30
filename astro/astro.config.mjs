c0379231881d8bd43c43c309379ab2ed9125cfc5
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-05-30 14:32:41 -0600

    portable text setup

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import preact from "@astrojs/preact";
 
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-sanity-minimal-starter.netlify.app/', // Your public domain, e.g.: https://my-site.dev/
-  integrations: [sitemap()]
+  site: 'https://astro-sanity-minimal-starter.netlify.app/',
+  // Your public domain, e.g.: https://my-site.dev/
+  integrations: [sitemap(), preact(), lit()]
 });
---
+++ astro/src/lib/sanityPortableText.js
@@ -0,0 +1,29 @@
+import { toHTML } from '@portabletext/to-html';
+import { getSanityImageURL } from '../utils/helpers.js';
+
+const customComponents = {
+  types: {
+    mainImage: ({ value }) => {
+      return html`
+        <picture>
+          <source
+            srcset="${getSanityImageURL(value.asset).format('webp').url()}"
+            type="image/webp"
+          />
+          <img
+            class="responsive__img"
+            src="${getSanityImageURL(value.asset).url()}"
+            alt="${value.alt}"
+          />
+        </picture>
+      `;
+    },
+    code: ({ value }) => {
+      return html`<code-block code=${value.code} language=${value.language}></code-block>`;
+    },
+  },
+};
+
+export function sanityPortableText(portabletext) {
+  return toHTML(portabletext, { components: customComponents });
+}
