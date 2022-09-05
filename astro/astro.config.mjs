0233c7beb84525025d7c5244f0b73b297c4ae977
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-05 14:01:49 -0600

    removed unncessary packages

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import preact from "@astrojs/preact";
-
-import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-sanity-minimal-starter.netlify.app/',
   // Your public domain, e.g.: https://my-site.dev/
-  integrations: [sitemap(), preact(), lit()]
+  integrations: [sitemap()]
 });
